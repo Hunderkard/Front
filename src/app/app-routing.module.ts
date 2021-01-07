567ed343ecbb74c31fb666fc0fd34a5de33e1b50
Hunderkard <juanmanuelmartel1996@gmail.com>
2021-01-07 18:39:59 +0100

    Navbar y Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// ti COMPONENTES
 
-const routes: Routes = [];
+import { LoginComponent } from './component/formularios/login/login.component';
+import { RegisterComponent } from './component/formularios/register/register.component';
+import { FormIngredienteComponent } from './component/formularios/form-ingrediente/form-ingrediente.component';
+
+// ha PARA SEPARAR LOS COMPOENTES PUBLICOS DE LOS PRIVADOS
+import { PublicComponent } from './component/public/public.component';
+import { SecureComponent } from './component/secure/secure.component';
+import { HomeComponent } from './component/public/home/home.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component:PublicComponent,
+    children:[
+      {path:"", component:HomeComponent},
+      {path:"login", component:LoginComponent},
+    ]
+  
+  },
+  {path:"secure", component:SecureComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"ingrediente/create", component:FormIngredienteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// ti MIS COMPONENTES
+import { RegisterComponent } from './component/formularios/register/register.component';
+import { CabeceraComponent } from './component/layout/cabecera/cabecera.component';
+
+// ti FORMULARIO REACTIVE.
+/* Thu-17/12 20:21:42 Decido usar los fromularios Reactive para no cargar de lógica el HTML. */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormIngredienteComponent } from './component/formularios/form-ingrediente/form-ingrediente.component';
+
+// ti ÉSTOS SE HAN PUESTOS SOLOS, ME DA MIEDO BORRARLOS.
+import { environment } from '../environments/environment';
+import { SecureComponent } from './component/secure/secure.component';
+
+// ti MIS MÓDULOS.
+import { PublicModule } from './component/public/public.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    CabeceraComponent,
+    FormIngredienteComponent,
+    SecureComponent,
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PublicModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
